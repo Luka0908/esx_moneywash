74d8b0bac1d90e05424af6dae00ecbf984638f17
Luka0908 <139494489+Luka0908@users.noreply.github.com>
2024-07-21 19:08:05 +0200

    Add files via upload

---
+++ esx_moneywash/locales/en.lua
@@ -0,0 +1,13 @@
+Locales['en'] = {
+	['you_have_washed'] 		= 'Oprali ste se ~r~$',
+	['you_have_washed_waiting'] = 'You have ~r~Opraliste se cekajuci. ~s~Time:~b~  ',
+	['dirty_money'] 			= ' Prjlav novac.',
+	['you_have_received'] 		= ' ~s~Dobili Ste Ukupno ~g~$',
+	['clean_money'] 			= ' 'Cistog Novca,
+	['invalid_amount']			= 'You have entered an invalid amount',
+	['press_menu'] 				= 'press ~INPUT_CONTEXT~ to ~r~wash your money.~s~',
+	['no'] 						= 'ne',
+	['wash_money']				= 'Opran Novac',
+	['washed_menu']				= ''Pranje Novca,
+	['wash_money_amount']		= 'Amount of money to wash'
+}
